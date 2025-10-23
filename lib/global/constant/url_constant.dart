8850bc97250d2f3e48b8afd5cc427c5f3a49d80a
alim ulas <alimulas20@gmail.com>
2025-10-23 11:32:50 +0300

    prediction url added

--- lib/global/constant/url_constant.dart
+++ lib/global/constant/url_constant.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class UrlConstants {
   static const String baseUrl = "https://pms-demo.smartplant360.com";
-
+static const String predictionPath = "services/prediction";
   // API endpoint paths
   static const String loggerPath = "/services/logger";
   static const String loggerTestPath = "/services/loggerTest";
@@ -16,7 +16,9 @@ class UrlConstants {
       return '$baseUrl$loggerPath';
     }
   }
-
+  static String getPredictionApiUrl(){
+    return '$baseUrl$predictionPath';
+  }
   /// Alternatif: Belirli bir endpoint için URL oluşturur
   static String buildUrl(String endpoint) {
     final String basePath = kDebugMode ? loggerTestPath : loggerPath;
